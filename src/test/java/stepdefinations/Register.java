f81363f5ff572392a703ac7cf3539c55a80dadd2
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-09 18:58:59 +0530

    Step Defination - Register.java

---
+++ src/test/java/stepdefinations/Register.java
@@ -0,0 +1,56 @@
+package stepdefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Register 
+{
+	@Given("user should navigates to the registration page")
+	public void user_should_navigates_to_the_registration_page() 
+	{
+	    
+	}
+
+	@When("user enters the details into below fields")
+	public void user_enters_the_details_into_below_fields(io.cucumber.datatable.DataTable dataTable) 
+	{
+	    
+	}
+
+	@When("user checks in user policy")
+	public void user_checks_in_user_policy() 
+	{
+	    
+	}
+
+	@When("user clicks on continue button")
+	public void user_clicks_on_continue_button() 
+	{
+	    
+	}
+
+	@Then("user should be able to view the successful registration message")
+	public void user_should_be_able_to_view_the_successful_registration_message() 
+	{
+	    
+	}
+
+	@When("user selects Yes for newsletter")
+	public void user_selects_yes_for_newsletter() 
+	{
+	   
+	}
+
+	@Then("user should be able to view the appropriate error message")
+	public void user_should_be_able_to_view_the_appropriate_error_message() 
+	{
+	    
+	}
+
+	@When("user does not enter any details")
+	public void user_does_not_enter_any_details() 
+	{
+	    
+	}
+}
