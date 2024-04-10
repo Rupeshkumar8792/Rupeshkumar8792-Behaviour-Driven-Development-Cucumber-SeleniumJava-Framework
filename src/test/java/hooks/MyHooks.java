385dd9d30e8b18449b05231014374d2faee7f6e3
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-10 05:35:52 +0530

    Driver manage and hooks class

---
+++ src/test/java/hooks/MyHooks.java
@@ -0,0 +1,30 @@
+package hooks;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+
+import driverFactory.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class MyHooks 
+{
+	WebDriver driver;
+	@Before
+	public void setUp()
+	{
+		DriverFactory.initializeBrowser("chrome");
+		driver = DriverFactory.getDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+	}
+	
+	@After
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
