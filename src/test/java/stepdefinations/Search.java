6b97b31d9fd5ef632c9f99dcdea1fc9c825d5ba5
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-10 09:29:24 +0530

    Search.java

--- src/test/java/stepdefinations/Search.java
+++ src/test/java/stepdefinations/Search.java
@@ -1,38 +1,47 @@
 package stepdefinations;
+import static org.testng.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import driverFactory.DriverFactory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Search
 {
-	@Given("user should navigates to the dashboard page")
-	public void user_should_navigates_to_the_dashboard_page() 
+	WebDriver driver = DriverFactory.getDriver();
+	
+	@Given("user launchs the application")
+	public void user_launchs_the_application() 
 	{
-	    
+	  
 	}
 
 	@When("User enter the valid product {string} into the search bar")
 	public void user_enter_the_valid_product_into_the_search_bar(String string) 
 	{
-	    
+	    driver.findElement(By.xpath("//div[@id=\"search\"]//input")).sendKeys(string);
 	}
 
 	@When("user clicks on the search Button")
 	public void user_clicks_on_the_search_button() 
 	{
-	   
+	   driver.findElement(By.xpath("//button[@class=\"btn btn-default btn-lg\"]")).click();
 	}
 
 	@Then("user should be able to view the HP product")
 	public void user_should_be_able_to_view_the_hp_product() 
 	{
-	    
+	    Assert.assertEquals(true, driver.findElement(By.linkText("HP LP3065")).isDisplayed());
 	}
 
 	@Then("user should be able to view the appropriate message")
 	public void user_should_be_able_to_view_the_appropriate_message() 
 	{
-	   
+	   Assert.assertEquals(true, driver.findElement(By.xpath("//p[text()='There is no product that matches the search criteria.']")).isDisplayed());
 	}
 
 	@When("User does not enter the any product into the search bar")
--- src/test/resources/features/Search.feature
+++ src/test/resources/features/Search.feature
@@ -1,19 +1,19 @@
 Feature: Search Functionality
  
  Scenario: Verify and Validate the search functionality with valid details
- Given user should navigates to the dashboard page
+ Given user launchs the application
  When User enter the valid product "HP" into the search bar
  And user clicks on the search Button
  Then user should be able to view the HP product
  
   Scenario: Verify and Validate the search functionality with Invalid details
- Given user should navigates to the dashboard page
+ Given user launchs the application
  When User enter the valid product "HONDA" into the search bar
  And user clicks on the search Button
  Then user should be able to view the appropriate message
  
   Scenario: Verify and Validate the search functionality without entering any details
- Given user should navigates to the dashboard page
+ Given user launchs the application
  When User does not enter the any product into the search bar
  And user clicks on the search Button
  Then user should be able to view the appropriate message 
