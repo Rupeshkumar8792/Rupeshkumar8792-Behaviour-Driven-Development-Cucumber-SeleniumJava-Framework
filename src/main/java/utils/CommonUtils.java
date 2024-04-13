0093a88d00e3c6136d01605748394c8cd4b0ae98
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-13 12:19:23 +0530

    elements utils page

--- src/main/java/utils/CommonUtils.java
+++ src/main/java/utils/CommonUtils.java
@@ -1,9 +1,13 @@
 package utils;
 
 import java.util.Date;
+import java.util.Properties;
 
 public class CommonUtils 
-{
+{	
+	public static final int implicitWait = 10;
+	public static final int explicitWait = 20;
+	
 	public String getEmailwithTimestamps()
 	{
 		Date date = new Date();
